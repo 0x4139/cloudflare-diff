959b7525050b168543dc6a3550eeac9a30ce1ddd
Vali Malinoiu <vali@Valis-MacBook-Pro.local>
2015-07-10 12:53:00 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# cloudflare-diff
+a small utility that updates the zone of a given domain based on the `dnsZone.json` file
+## Installation
+
+`npm i --save cloudflare-diff`
+## Usage
+
+`DOMAIN=0x4139.com EMAIL=0x4139@gmail.com TOKEN=your_token node cloudflare-diff/index.js`
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Todo
+    - make it look more like a library
+    - more functionality in the json like the record type (A/CNAME/TXT.. etc)
+## History
+
+TODO: Write history
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+var CloudflareAPI = require('cloudflare-api');
+
+var dns_zone = require('./dnsZone.json');
+var client = new CloudflareAPI({
+    email: process.env.EMAIL,
+    token: process.env.TOKEN
+});
+
+client.execute({
+    a: 'rec_load_all',
+    z: process.env.DOMAIN,
+    type: 'A'
+}).then(function (result) {
+
+    //update what is necessary
+    result.response.recs.objs.forEach(function (record) {
+        if (dns_zone[record.display_name] && dns_zone[record.display_name] != record.content) {
+            console.log('updating ', record.display_name);
+            client.execute({
+                a: 'rec_edit',
+                id: record.rec_id,
+                z: process.env.DOMAIN,
+                content: dns_zone[record.display_name],
+                type: 'A',
+                name: record.display_name,
+                ttl: '120'
+            }).catch(console.log);
+        }
+        delete dns_zone[record.display_name];
+    });
+    //insert what is new;
+    for (var subdomain in dns_zone) {
+        if (dns_zone.hasOwnProperty(subdomain)) {
+            console.log('inserting ', subdomain);
+            client.execute({
+                a: 'rec_new',
+                z: process.env.DOMAIN,
+                content: dns_zone[subdomain],
+                type: 'A',
+                name: subdomain,
+                ttl: '120'
+            }).catch(console.log);
+        }
+    }
+});
